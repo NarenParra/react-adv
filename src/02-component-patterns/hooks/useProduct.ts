9720c258b87ccd7c2183ecd536f35394a8635807
Naren Parra <narenparra@MacBook-Air-de-Naren.local>
2022-05-18 11:58:42 -0500

    control props alternativo

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { OnChangeArgs, Product } from "../interfaces/interfaces";
 
 interface UseProductArgs {
@@ -13,17 +13,8 @@ export const useProduct = ({
   value = 0,
 }: UseProductArgs) => {
   const [counter, setCounter] = useState(value);
-  //para saber si nuestro componente esta siendo controlado
-  //y tambien para eviar renderizados innececesarios
-  //el doble simbolo de admiracion transforma a un true o false
-  const isControlled = useRef(!!onChange);
 
   const increaseBy = (value: number) => {
-    //para obtener la referencia de un useref se debe utilizar el .current
-    // console.log("isControlled", isControlled);
-    if (isControlled.current) {
-      return onChange!({ count: value, product });
-    }
     const newValue = Math.max(counter + value, 0);
     setCounter(newValue);
     onChange && onChange({ count: newValue, product });
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -15,28 +15,15 @@ export const useShoppingCart = () => {
   }) => {
     //    console.log("on product count change", count, product);
     setshoppingCart((oldShoppingCart) => {
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || {
-        ...product,
-        count: 0,
-      };
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldShoppingCart,
-          [product.id]: productInCart,
-        };
+      //eliminar del estado cuando no hay productos
+      if (count === 0) {
+        //por medio de la destructuracion
+        //saca el producto con el id y lo elimina y retorna el resto
+        const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+        return rest;
       }
-      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-      return rest;
-      /*       //eliminar del estado cuando no hay productos
-    if (count === 0) {
-      //por medio de la destructuracion
-      //saca el producto con el id y lo elimina y retorna el resto
-      const { [product.id]: toDelete, ...rest } = oldShoppingCar;
-      return rest;
-    }
 
-    return { ...oldShoppingCar, [product.id]: { ...product, count } }; */
+      return { ...oldShoppingCart, [product.id]: { ...product, count } };
     });
   };
 
